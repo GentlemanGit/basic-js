fcdb3f99648ce0fcfcd8b116c36775306d9a0502
Ivan <78233139+GentlemanGit@users.noreply.github.com>
2021-02-24 15:38:04 +0300

    feat: complete task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {// проверка на массив
+    let arrStr = [...arr];// переводим в массив строк. для правильного вывода
+    let item = 0;//номер элемента массива
+
+    for (let i = 0; i < arrStr.length; i++) {//проходим массив
+      if (arr[i] === "--double-next") {
+        item = i;
+        arrStr.splice(item, 1, arr[item + 1]);// вырезаем "--double-next" и дублируем следующий элемент
+      }
+      if (arr[i] === "--double-prev") {
+        item = i;
+        arrStr.splice(item, 1, arrStr[item - 1]);// вырезаем "--double-prev" и дублируем предыдущий элемент
+      }
+      if (arr[i] === "--discard-prev") {
+        if (i > 0) {// проверка на то что элемент не первый
+          item = i - 1;
+          arrStr.splice(item, 1, undefined);// вырезаем "--discard-prev" и заменяем на undefined
+        }
+      }
+      if (arr[i] === "--discard-next") {
+        if (i < arr.length) {// проверка на то что элемент не последний
+          item = i + 1;
+          arrStr.splice(item, 1, undefined);// вырезаем "--discard-next" и заменяем на undefined
+        }
+      }
+    }
+    let Transform_array = arrStr.filter(// проверка на правильность выполнения программы. И удаление лишнего мусора
+      (item) =>
+        item !== undefined &&
+        item !== "--discard-next" &&
+        item !== "--discard-prev" &&
+        item !== "--double-prev" &&
+        item !== "--double-next"
+    );
+    return Transform_array;
+  } else throw Error("arr is not an Array");// вывод ошибки
 };
