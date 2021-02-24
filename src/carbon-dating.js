4bc44fbcb31daef352c81af018cca985380466b0
Ivan <78233139+GentlemanGit@users.noreply.github.com>
2021-02-24 15:33:54 +0300

    feat: complete task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const SA = +sampleActivity; //Переводим строку в число. Если строка кривая получим NaN
+  if (!isNaN(SA)) {
+    //Проверяем на NaN
+    Math.log(MODERN_ACTIVITY / SA) / (0.693 / HALF_LIFE_PERIOD); //Формула рассчёта
+    if (!isNaN(SA)) return false;
+  } // Ещё раз проверяем на NaN если с числом было всё впорядке. однако оно было отриц то из за log получим NaN
+  else return false;
 };
